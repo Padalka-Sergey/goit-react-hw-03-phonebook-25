97fa222b6c7cddb8e1f97d9176b61a4bcb8f68fc
Padalka-Sergey <106059712+Padalka-Sergey@users.noreply.github.com>
2025-04-29 22:40:15 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,26 +6,13 @@ import { ContactForm } from 'components/ContactForm/ContactForm.jsx';
 import { Filter } from 'components/Filter/Filter.jsx';
 import { ContactList } from 'components/ContactList/ContactList.jsx';
 
+const LS_KEY = 'contacts';
+
 export class App extends Component {
   state = {
     contacts: contactsData,
     filter: '',
   };
-
-  componentDidMount() {
-    const savedContacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(savedContacts);
-
-    if (parsedContacts.length > 0) {
-      this.setState({ contacts: parsedContacts });
-    }
-  }
-
-  componentDidUpdate(_, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
 
   formSubmitHandler = data => {
     this.setState(prevState => ({
@@ -49,6 +36,21 @@ export class App extends Component {
     );
     this.setState({ contacts: newContacts });
   };
+
+  componentDidMount() {
+    const savedContacts = localStorage.getItem(LS_KEY);
+    const parsedContacts = JSON.parse(savedContacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem(LS_KEY, JSON.stringify(this.state.contacts));
+    }
+  }
 
   render() {
     const { contacts, filter } = this.state;
